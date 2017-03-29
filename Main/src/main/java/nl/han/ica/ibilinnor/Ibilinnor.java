98f7ba76edd11c308dc7a527fb419ccb0bd6eda6
JoepvE <Joep0702@live.nl>
2017-03-29 14:49:13 +0200

    methods added to main

--- Main/src/main/java/nl/han/ica/ibilinnor/Ibilinnor.java
+++ Main/src/main/java/nl/han/ica/ibilinnor/Ibilinnor.java
@@ -1,10 +1,35 @@
 package nl.han.ica.ibilinnor;
 
-public class Ibilinnor {
+import nl.han.ica.OOPDProcessingEngineHAN.Dashboard.Dashboard;
+import nl.han.ica.OOPDProcessingEngineHAN.Engine.GameEngine;
+import nl.han.ica.OOPDProcessingEngineHAN.Objects.Sprite;
+import nl.han.ica.OOPDProcessingEngineHAN.Persistence.FilePersistence;
+import nl.han.ica.OOPDProcessingEngineHAN.Persistence.IPersistence;
+import nl.han.ica.OOPDProcessingEngineHAN.Sound.Sound;
+import nl.han.ica.OOPDProcessingEngineHAN.Tile.TileMap;
+import nl.han.ica.OOPDProcessingEngineHAN.Tile.TileType;
+import nl.han.ica.OOPDProcessingEngineHAN.View.EdgeFollowingViewport;
+import nl.han.ica.OOPDProcessingEngineHAN.View.View;
+import nl.han.ica.waterworld.tiles.BoardsTile;
+import processing.core.PApplet;
+
+public class Ibilinnor extends GameEngine{
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+	}
+
+	@Override
+	public void setupGame() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void update() {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
