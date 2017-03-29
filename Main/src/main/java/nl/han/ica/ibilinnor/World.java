8cbcd3c75d645643b9d89021e80d3a026813504b
Tobias <t.cunnen@gmail.com>
2017-03-29 14:52:14 +0200

    class world added

---
+++ Main/src/main/java/nl/han/ica/ibilinnor/World.java
@@ -0,0 +1,23 @@
+package nl.han.ica.ibilinnor;
+
+import nl.han.ica.OOPDProcessingEngineHAN.Engine.GameEngine;
+
+public class World extends GameEngine {
+
+	public World() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void setupGame() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void update() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
