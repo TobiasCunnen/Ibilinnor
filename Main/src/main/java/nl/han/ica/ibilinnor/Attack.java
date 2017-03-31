731669388d6dbe309498c69257dcd8436322a3f2
JoepvE <Joep0702@live.nl>
2017-03-31 15:57:45 +0200

    attack class

---
+++ Main/src/main/java/nl/han/ica/ibilinnor/Attack.java
@@ -0,0 +1,24 @@
+package nl.han.ica.ibilinnor;
+
+import nl.han.ica.OOPDProcessingEngineHAN.Objects.AnimatedSpriteObject;
+import nl.han.ica.OOPDProcessingEngineHAN.Objects.Sprite;
+
+public class Attack extends AnimatedSpriteObject {
+
+	private int x;
+	private int y;
+	
+	public Attack(Sprite sprite, int totalFrames) {
+		super(sprite, totalFrames);
+		this.x = x;
+		this.y = y;
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void update() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
